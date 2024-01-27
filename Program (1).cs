8282025217aa10918b4f2ae028bf683795fe76db
stacy2906 <124590794+stacy2906@users.noreply.github.com>
2024-01-27 14:25:19 +0200

    Add files via upload

---
+++ Program (1).cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //Задание 1
+            //DateTime dateTime4 = DateTime.Now;
+            //Console.WriteLine("Текущая дата:");
+            //Console.WriteLine(dateTime4.ToString("d.MM.yyyy"));
+            //Console.WriteLine(dateTime4.ToLongDateString());
+            //Console.WriteLine(dateTime4.ToShortDateString());
+            //Console.WriteLine($"{dateTime4:d}");
+            //Console.WriteLine(dateTime4.ToString("d MMM yyyy"));
+            //Console.WriteLine(dateTime4.ToString("d MMMM yyyy") + " г.");
+            //Console.WriteLine(dateTime4.ToString("ddd, d MMM yyyy"));
+            //Console.WriteLine(dateTime4.ToString("dddd, d MMMM yyyy"));
+
+            //DateTime dateTime3 = DateTime.Now;
+            //Console.WriteLine("Текущее время:");
+            //Console.WriteLine(dateTime3.ToString("HH:mm:ss"));
+            //Console.WriteLine(dateTime3.ToString("HH:mm"));
+
+            //Console.WriteLine("Введите возраст:");
+            //int age = Convert.ToInt32(Console.ReadLine());
+            //int birthYear = DateTime.Now.Year - age;
+            //Console.WriteLine("Ты родился в " + birthYear + " году");
+
+            // Задание 2
+            //Console.WriteLine("Введите возраст:");
+            //int age = Convert.ToInt32(Console.ReadLine());
+            //int birthYear = DateTime.Now.Year - age;
+            //Console.WriteLine("Ты родился в " + birthYear + " году");
+
+            // Задание 3
+            //Console.WriteLine("Введите год рождения:");
+            //int birthYearInput = Convert.ToInt32(Console.ReadLine());
+            //int currentYear = DateTime.Now.Year;
+            //int personAge = currentYear - birthYearInput;
+            //Console.WriteLine("Вам " + personAge + " год(а/лет)");
+            //Console.ReadKey();
+        }
+    }
+ }
+
